953b120114639bcab511c6fa2dac3279014784b7
Ritik Singh <ritiks236@gmail.com>
2024-08-06 20:02:51 +0530

    Updated user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface UserProps extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<UserProps>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model<UserProps>("User", userSchema);
