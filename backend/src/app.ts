0d77252f33744fb65c8865af33651f35adb476eb
ritiks236 <ritiks236@gmail.com>
2024-08-14 00:55:11 +0530

    Connected routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,25 @@
 import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+import Task from "./models/task";
+import rootRouter from "./routes/index";
+
 const app: Application = express();
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
+connectDB(); // connecting to database
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello app is running ");
+app.use(express.json());
+
+app.use("/api/v1", rootRouter);
+
+app.get("/", async (req: Request, res: Response) => {
+  const users = await Task.find();
+  res.json(users);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
